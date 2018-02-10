6cc2e86b78887ed39b72c7aa2db14d0a4f4a5058
Olufemi Salaam <salaam.olufemi@gmail.com>
2018-02-10 16:25:58 -0500

    Opens a file from

--- DCHQuestionAlg.cpp
+++ DCHQuestionAlg.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
+    ifstream myfile;
+    myfile.open(argv[1]);//opens the file name provided on the command line
     
 }
